a27f5d02427a0ad2429ede88b23bdc5bc6182b38
Tanish0905 <tanishpadvekar@gmail.com>
2023-04-10 14:44:51 +0530

    level 2: encryption of passwords(database)

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+//installed npm i
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt =require('mongoose-encryption');
 const port = 3001;
  
 const app = express();
@@ -13,10 +15,15 @@ app.set('view engine', 'ejs');
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true }); 
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String ,
     password: String
-};
+});
+
+
+const secret ="Thisisourlittlesecrete.";
+userSchema.plugin(encrypt,{secret:secret, encryptedFields: ["password"]})
+
 
 const User = new mongoose.model("User",userSchema);
  
